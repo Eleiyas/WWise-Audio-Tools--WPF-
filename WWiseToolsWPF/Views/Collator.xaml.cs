e7596d9ca3465f839893ec5d0c4183c7cd0257b0
Eleiyas <els236@rocketmail.com>
2026-04-20 13:09:40 +0100

    Update Collator.xaml.cs

--- WWiseToolsWPF/Views/Collator.xaml.cs
+++ WWiseToolsWPF/Views/Collator.xaml.cs
@@ -130,8 +130,8 @@ namespace WWiseToolsWPF.Views
 
             var result = new ConcurrentBag<string>();
 
-            string[] keyNames = { "_sourceNames", "SourceNames", "sourceNames", "OFEEIPOMNKD", "EIKJKDICKMJ", "DHMACMBAEHG", "FOLFEPNIKEC", "FONIFKPLDGC", "HLGDIILMGCB", "LPFADPAJNJE", "JKHGLBHOKIC", "JKDJFGBGOEB", "OPGDOEDEJOJ" };
-            string[] fieldNames = { "sourceFileName", "CBGLAJNLFCB", "HLGOMILNFNK", "NCPBJNJNCEI", "HPAJGPIFDKB", "POLDPGADMOJ", "KEGGFHAFNBM", "AJHGGOIEIFN", "BJDAJEKPCFP", "DCIHFJLBLAP", "MDOCAGOFPAP" };
+            string[] keyNames = { "_sourceNames", "SourceNames", "sourceNames", "OFEEIPOMNKD", "EIKJKDICKMJ", "DHMACMBAEHG", "FOLFEPNIKEC", "FONIFKPLDGC", "HLGDIILMGCB", "LPFADPAJNJE", "JKHGLBHOKIC", "JKDJFGBGOEB", "OPGDOEDEJOJ", "NEJBKIJIBHJ" };
+            string[] fieldNames = { "sourceFileName", "CBGLAJNLFCB", "HLGOMILNFNK", "NCPBJNJNCEI", "HPAJGPIFDKB", "POLDPGADMOJ", "KEGGFHAFNBM", "AJHGGOIEIFN", "BJDAJEKPCFP", "DCIHFJLBLAP", "MDOCAGOFPAP", "NKPOOPONEHG" };
 
             await Task.WhenAll(files.Select(async fileName =>
             {
